3abd2f8cbd2e25b2ce841ed19f43da5378ceba5b
David Fulop <david.fulop@7digital.com>
2018-04-04 13:56:02 +0100

    Decrease wait before starting the tests.

--- tests/Transcoder.SmokeTests/container-start.sh
+++ tests/Transcoder.SmokeTests/container-start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-bash tests/Transcoder.SmokeTests/wait-for-it.sh -t 9 http://gcf-local/functionstest/us-central1/trackGET
+bash tests/Transcoder.SmokeTests/wait-for-it.sh -t 8 http://gcf-local/functionstest/us-central1/trackGET
 dotnet test tests/Transcoder.SmokeTests/Transcoder.SmokeTests.csproj --no-build
