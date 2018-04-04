4e134f8067c0fc738406972fdfe3570147222ba3
David Fulop <david.fulop@7digital.com>
2018-04-04 14:09:52 +0100

    Delete comments.

--- src/index.js
+++ src/index.js
@@ -11,13 +11,11 @@ var fs = require('fs');
 exports.trackGET = (req, res) => {
     res.setHeader("content-type", "audio/ogg");
     var converter = ffmpeg();
- 
-    // get a writable input stream and pipe an image file to it 
+
     var input = converter.input({f: 'flac'});
     request('http://test-server:80/media/0', function (error, response, body) {
     }).pipe(input);
-     
-    // create an output stream, crop/scale image, save to file via node stream 
+    
     converter.output({f: 'oga', acodec: 'libvorbis'})
     .pipe(res);
      
